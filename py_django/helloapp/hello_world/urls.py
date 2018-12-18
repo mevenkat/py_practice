aa1f5cf64128f275c21fc88ee19c9e5e4bb10cfd
Venkatesh Manubolu <mevenkat@users.noreply.github.com>
2018-12-18 16:28:13 +0530

    Add files
    Add files

---
+++ py_django/helloapp/hello_world/urls.py
@@ -0,0 +1,10 @@
+# hello_world/urls.py
+from django.conf.urls import url
+from hello_world import views
+
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()),  # Add this /about/ route
+
+]
---
+++ py_test/server.py
@@ -0,0 +1,12 @@
+def f1(a, b):
+    c = a
+    d = b
+    return c+d
+
+
+# print('Results of ^a and ^b is',f1(10,20))
+a, b = 10, 20
+a = a + b
+b = a - b
+a = a - b
+print(a, b)
